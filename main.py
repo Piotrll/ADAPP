5e9ccba278176156996c9b78c6709bca50db476c
Piotrll <pio.lew.pml@gmail.com>
2023-01-29 16:55:28 +0100

    Poprawki działanie i nazwy/komentarze

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ def main_start():
     setup = ConfigParser()
     setup.read("conf.ini")
     initial_color = setup.get('THEME', 'color')
-    customtkinter.set_appearance_mode("dark")
-    customtkinter.set_default_color_theme(initial_color) #To bedzie można potem zmienić w ustawieniach
+    initial_appearance = setup.get('THEME', 'appearance')
+    customtkinter.set_appearance_mode(initial_appearance)
+    customtkinter.set_default_color_theme(initial_color) #To można zmienić w ustawieniach
 
     root = customtkinter.CTk() #Główne pole działania(kontener), lepiej nie tykac
     root.geometry("500x350")
@@ -26,7 +27,7 @@ def show(widget): #A tu magicznie przywracać
 def menu(root):
     def login():
         def login_action():
-            #Tu ma być logika bazy danych logowania
+            # Tu ma być logika bazy danych logowania
             if entry1.get() == "user" and entry2.get() == "passwd": #Test działania - check
                 print("ol")
             else:
@@ -38,14 +39,14 @@ def menu(root):
 
         hide(frame_main)
         # chowanie menu
-        #Panel logowania należy do login()
+        # Panel logowania należy do login()
         frame = customtkinter.CTkFrame(master=root)
         frame.pack(pady=20, padx=10, fill="both", expand=True)
 
         back = customtkinter.CTkButton(master=frame, text="Back", command=lambda: [hide_frame(), menu(root)]) #Powrót do menu, usuwa frame i odpala główną funcje menu od nowa
         back.pack(pady=12, padx=10)
 
-        label = customtkinter.CTkLabel(master=frame, text="Login") #Napis nad panelem logowania
+        label = customtkinter.CTkLabel(master=frame, text="Login")  # Napis nad panelem logowania
         label.pack(pady=12, padx=10)
 
         entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
@@ -54,37 +55,45 @@ def menu(root):
         entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
         entry2.pack(pady=12, padx=10)
 
-        button_log = customtkinter.CTkButton(master=frame, text="login", command=login_action) #Przycisk login odwołuje do login_action() czyli skrypt łaczenia do bazy
+        button_log = customtkinter.CTkButton(master=frame, text="login", command=login_action)  #Przycisk login odwołuje do login_action() czyli skrypt łaczenia do bazy
         button_log.pack(pady=12, padx=10)
 
         checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember me")
         checkbox.pack(pady=10, padx=10)
 
     def settings():
-        def save_set(value):
+        def save_set(coloor, appearance):
             conf = ConfigParser()
             conf["THEME"] = {
-                "color": value
+                "color": coloor,
+                "appearance": appearance
             }
             with open("conf.ini", "w") as w:
                 conf.write(w)
-        def theme_change(value):
+
+        def color_change(value):
             customtkinter.set_default_color_theme(value)
+
+        def theme_change(value):
+            customtkinter.set_appearance_mode(value)
         hide(frame_main)
 
         frame_set = customtkinter.CTkFrame(master=root)
         frame_set.pack(pady=20, padx=10, fill="both", expand=True)
 
-        label = customtkinter.CTkLabel(master=frame_set, text="Main Color")  # Napis nad panelem logowania
+        label = customtkinter.CTkLabel(master=frame_set, text="Main theme")  # Napis nad panelem logowania
         label.pack(pady=12, padx=10)
 
-        theme = customtkinter.CTkSegmentedButton(master=frame_set,dynamic_resizing=True, values=["blue", "green"], command=theme_change)
+        theme = customtkinter.CTkSegmentedButton(master=frame_set,dynamic_resizing=True, values=["blue", "green", "dark-blue"], command=color_change)
         theme.pack(pady=12, padx=10)
 
-        back = customtkinter.CTkButton(master=frame_set, text="Apply", command=lambda: [save_set(theme.get()), hide(frame_set), menu( root)])  # Powrót do menu, usuwa frame i odpala główną funcje menu od nowa
+        appearance = customtkinter.CTkSegmentedButton(master=frame_set, dynamic_resizing=True, values=["dark", "light", "system"], command=theme_change)
+        appearance.pack(pady=12, padx=10)
+
+        back = customtkinter.CTkButton(master=frame_set, text="Apply", command=lambda: [save_set(theme.get(), appearance.get()), hide(frame_set), menu(root)])  # Powrót do menu, usuwa frame i odpala główną funcje menu od nowa
         back.pack(pady=12, padx=10)
 
-#panel menu poniżej należy do menu(root)
+    #panel menu poniżej należy do menu(root)
     frame_main = customtkinter.CTkFrame(master=root)
     frame_main.pack(pady=20, padx=10, fill="both", expand=True)
 
